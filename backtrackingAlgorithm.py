9b1364a4c6c5e79b78a6ac59884566d540195ef9
Graham Finlayson-Fife <graham@finlayson-fife.org>
2023-12-24 22:22:55 -0500

    init

---
+++ backtrackingAlgorithm.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+class Schedule:
+
+    def __init__(self):
+        self.solution = [[None for _ in range(num_timeslots)] for _ in range(num_courses)]
+        self.constraints = []
+        self.load_constraints()
+        
+    def is_valid(self, course, timeslot):  
+        # Check if assigning course to timeslot satisfies constraints
+        return True
+
+    def solve(self):
+        if self.backtrack():
+            return self.solution
+        return None
+    
+    def backtrack(self):
+        if self.is_complete(): 
+            return True
+        course = self.get_unassigned_var() 
+        for timeslot in range(num_timeslots):
+            if self.is_consistent(course, timeslot):
+                self.solution[course][timeslot] = True 
+                result = self.backtrack()
+                if result:
+                    return True
+                self.solution[course][timeslot] = None
+        return False
+
+    def get_unassigned_var(self):
+        # Return next unassigned course
+
+    def is_complete(self):
+        # Return True if all courses are assigned timeslots
+        
+    def is_consistent(self, course, timeslot):
+        # Check if assigning course to timeslot is valid
+        if self.is_valid(course, timeslot):
+            return True
+        return False
