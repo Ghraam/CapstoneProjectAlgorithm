6d8d52e836b7aba958ac494474b13a2395a7b491
Ghraam <terbom@protonmail.com>
2024-03-20 15:32:21 -0400

    try has_one instead of belongs to

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,7 +1,7 @@
 class Section < ApplicationRecord
   belongs_to :course
   belongs_to :professor
-  belongs_to :time_block, through: :start
-  belongs_to :time_block, through: :end
+  has_one :time_block, through: :start
+  has_one :time_block, through: :end
   belongs_to :classroom
 end
