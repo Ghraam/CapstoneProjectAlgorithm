db423a43781d58acffed4c7f63411a639501b10f
Ghraam <terbom@protonmail.com>
2024-03-20 16:37:05 -0400

    fix ArgumentError

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   before_action :set_course, only: %i[ show edit update destroy ]
 
   # GET /courses or /courses.json
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :verify_authenticity_token  # Skip CSRF protection for the generate action
+  # skip_before_action :verify_authenticity_token
 
   # Generate the schedule (assignments) using a python backtracking algorithm
   def generate
